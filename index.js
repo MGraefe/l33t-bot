61501106aa189e3954f53efd30fa5a3539471972
Marius Gräfe <git@mgraefe.de>
2022-04-20 17:44:25 +0200

    Fixed incorrect l33t count

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const qrcode = require('qrcode');
 const WAWebJS = require('whatsapp-web.js');
 
 const GROUP_ID = process.env.L33TBOT_GROUP_ID;
+const QR_FILENAME = process.env.L33TBOT_QR_FILENAME || 'qr.png';
+
 const DAY_MS = 1000 * 60 * 60 * 24; // 1 day in milliseconds
 
 const client = new WAWebJS.Client({
@@ -13,8 +15,7 @@ const client = new WAWebJS.Client({
 let shutdownTimer = null;
 
 client.on('qr', (qr) => {
-  const qrFilename = process.env.L33TBOT_QR_FILENAME || 'qr.png';
-  qrcode.toFile(qrFilename, qr);
+  qrcode.toFile(QR_FILENAME, qr);
   console.log('received QR, please scan!');
 
   shutdownTimer = setTimeout(() => {
@@ -45,6 +46,8 @@ function reportResult(chat, counter) {
     outMsg = `${counter}'er Strähne!`;
   } else if (counter === 1) {
     outMsg = `${counter}...alles fängt mal klein an`;
+  } else if (counter === 2) {
+    outMsg = `${counter}...weiter so!`;
   } else {
     outMsg = `RIP STRÄHNE Sadge`;
   }
@@ -69,17 +72,22 @@ function countL33ts(chat, maxMsgCount = 50) {
     let l33tForDay = false;
     for (let msg of messages.reverse()) {
       const msgTime = new Date(msg.timestamp * 1000);
-      const diff = day - msgTime;
-      // was this on the given day?
-      if (diff > DAY_MS) {
-        if (!l33tForDay) {
+
+      // is this message already on the next day?
+      if ((day - msgTime) > DAY_MS) {
+        if (!l33tForDay) { // no leet for whole day? :(
           reportResult(chat, counter);
           return;
         }
+
         // roll over to next day
         day -= DAY_MS;
         l33tForDay = false;
-      } else if (
+      }
+      
+      // check for l33t messages if we haven't found one already,
+      // otherwise just ignore the messages
+      if (
         !l33tForDay &&
         msgTime.getHours() === 13 &&
         msgTime.getMinutes() === 37 &&
@@ -106,9 +114,14 @@ client.on('ready', () => {
 });
 
 
-client.on('auth_failure', () => {
-  console.error('Authentication error!');
+client.on('auth_failure', (msg) => {
+  console.error('Authentication error!', msg);
   process.exit(1);
+});
+
+
+client.on('disconnected', (reason) => {
+  console.log('Client was logged out', reason);
 });
 
 
